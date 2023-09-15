a7d3e99146c41997f7a6b59a342d2429b8cf2bf6
Finn Freitag <finn-freitag@web.de>
2023-09-15 22:58:20 +0200

    Designed GUI

--- CPUSimulator/Form1.cs
+++ CPUSimulator/Form1.cs
@@ -15,6 +15,14 @@ namespace CPUSimulator
         public Form1()
         {
             InitializeComponent();
+            this.Text = Strings.Title;
+            label1.Text = Strings.DataBus;
+            label2.Text = Strings.AddressBus;
+            label3.Text = Strings.Accumulator;
+            label4.Text = Strings.InstructionPointer;
+            label5.Text = Strings.InstructionRegister;
+            label8.Text = Strings.ALU;
+            button1.Text = Strings.Settings;
         }
     }
 }
---
+++ CPUSimulator/Strings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUSimulator
+{
+    public static class Strings
+    {
+        public static string Title = "CPU Simulator";
+        public static string DataBus = "Data bus";
+        public static string AddressBus = "Address bus";
+        public static string Accumulator = "Accumulator";
+        public static string InstructionRegister = "Instruction register";
+        public static string InstructionPointer = "Instruction pointer";
+        public static string ALU = "ALU";
+        public static string Settings = "Settings";
+    }
+}
